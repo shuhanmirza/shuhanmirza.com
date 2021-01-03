9d367c6feb15f9612b1c56ae1a47c2b71d6ac1f0
shuhanmirza <shuhan.mirza@gmail.com>
2021-01-03 09:30:10 +0600

    http to https redirection

--- index.php
+++ index.php
@@ -411,10 +411,12 @@
     </script>
 
     <?php
-        if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-            $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-            header('HTTP/1.1 301 Moved Permanently');
-            header('Location: ' . $location);
+        //If the HTTPS is not found to be "on"
+        if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
+        {
+            //Tell the browser to redirect to the HTTPS URL.
+            header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
+            //Prevent the rest of the script from executing.
             exit;
         }
     ?>
