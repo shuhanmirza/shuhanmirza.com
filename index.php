05c5be81e98b4e88e4188a8a170bec0c22562e5a
shuhanmirza <shuhan.mirza@gmail.com>
2021-01-03 09:24:23 +0600

    http to https redirection

--- index.php
+++ index.php
@@ -409,4 +409,14 @@
 
 
     </script>
+
+    <?php
+        if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+            $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+            header('HTTP/1.1 301 Moved Permanently');
+            header('Location: ' . $location);
+            exit;
+        }
+    ?>
+
 </html>
